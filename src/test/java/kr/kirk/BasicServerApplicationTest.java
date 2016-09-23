7a2a4445e3ba25ec362fe438c42b785126830a2e
Kirk77 <kirk77@gmail.com>
2016-09-23 16:46:53 +0900

    C:/Program Files/Git

--- src/test/java/kr/kirk/BasicServerApplicationTest.java
+++ src/test/java/kr/kirk/BasicServerApplicationTest.java
@@ -1,5 +1,14 @@
 package kr.kirk;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class BasicServerApplicationTest {
 
+	@Test
+	public void contextLoad() {
+		
+	}
 }
